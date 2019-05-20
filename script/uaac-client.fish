2be9783aabeb8a3ba0e6352a46044bfc9451c9b7
shinyay <shinya.com@gmail.com>
2019-05-20 23:28:14 +0900

    Modify hostname as real hostname from localhost

--- script/uaac-client.fish
+++ script/uaac-client.fish
@@ -6,7 +6,7 @@ docker run --rm -v (pwd):/root -it shinyay/cf-uaac client add webappclient -s we
 --scope resource.read,resource.write,openid,profile,email,address,phone \
 --authorized_grant_types authorization_code,refresh_token,client_credentials,password \
 --authorities uaa.resource \
---redirect_uri http://localhost:8081/login/oauth2/code/uaa
+--redirect_uri http://(hostname):8081/login/oauth2/code/uaa
 
 echo "[VALIDAE UAA CLIENT]"
 docker run --rm -v (pwd):/root -it shinyay/cf-uaac client get webappclient
