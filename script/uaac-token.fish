5a7b4b129373b55803835bcd7ddddbe90d6741d9
shinyay <shinya.com@gmail.com>
2019-05-22 12:58:18 +0900

    Modify remove old uaac.yml process

--- script/uaac-token.fish
+++ script/uaac-token.fish
@@ -1,4 +1,8 @@
 #/usr/bin/env fish
 
+if test -e .uaac.yml
+  rm .uaac.yml
+end
+
 docker run --rm -v (pwd):/root -it shinyay/cf-uaac target http://(hostname):8080/uaa
 docker run --rm -v (pwd):/root -it shinyay/cf-uaac token client get admin -s adminsecret
