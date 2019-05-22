147eec3f2257a2af3cd685a04d35dd1b63141098
shinyay <shinya.com@gmail.com>
2019-05-22 13:38:10 +0900

    Add key info as environment value

---
+++ script/setenv-uaa-server.fish
@@ -0,0 +1,12 @@
+#!/usr/bin/env fish
+
+if test -e signingkey.pem
+  openssl genrsa -out signingkey.pem 2048
+end
+if test -e verificationkey.pem
+  openssl rsa -in signingkey.pem -pubout -out verificationkey.pem
+end
+
+set -x SPRING_PROFILES "default,hsqldb"
+set -x JWT_TOKEN_SIGNING_KEY (cat signingkey.pem)
+set -x JWT_TOKEN_VERIFICATION_KEY (cat verificationkey.pem)
